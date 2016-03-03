b92cd7a566b341ad951c94765ecd35b84f6970b0
何晨煜Elton Ho <ck6tp6m4@gmail.com>
2016-03-03 17:15:08 +0800

    track path to make further analysis more convenient

--- handwrittenImage.js
+++ handwrittenImage.js
@@ -3,7 +3,7 @@ function handwrittenImage(canvas, src) {
   // load image in canvas
   var context = canvas.getContext('2d');
   var img = new Image();
-  img.crossOrigin = "Anonymous";  //disable this line if image is in same server.
+  img.crossOrigin = "Anonymous";  //disable this row if image is in same server.
   var that = this;
   img.onload = function(){
     canvas.width = img.width;
@@ -65,40 +65,36 @@ ImageData.prototype.blackPixels = function(calculateRightDown){
     for(var y = 0; y < h; y++) {
       // loop through each column
       for(var x = 0; x < w-1; x++) {  //not visiting last column cause no "right" point
-        if(data[((w * y) + x) * 4]===0){
+        if(data[((w * y) + x) * 4]===0 &&data[((w * y) + x) * 4 +3]===255){
           bp.amount++;
-          if(data[((w * y) + x+1) * 4]===0 && data[((w * (y+1)) + x) * 4]===0)// && data[((w * (y+1)) + x+1) * 4]===0)
+          if(data[((w * y) + x+1) * 4]===0 && data[((w * y) + x+1) * 4+3]===255 && data[((w * (y+1)) + x) * 4]===0 && data[((w * (y+1)) + x) * 4 +3]===255)// && data[((w * (y+1)) + x+1) * 4]===0)
             bp.rightDownBlack++;
         }
       }
       if(data[((w * y) + x) * 4]===0) bp.amount++;  //visit last column 
     }
   }else{
-    for(i = 0; i < n; i += 4) if(data[i]===0) bp.amount++;
+    for(i = 0; i < n; i += 4) if(data[i]===0 && data[i+3]===255) bp.amount++;
   }
   return bp;
 };
 
 handwrittenImage.prototype.viterbi = function(vertical){
 // Estimate stroke width
-  var iwidth = this.width;
+  var iwidth  = this.width;
   var iheight = this.height;
-  // var strokeWidth = this.strokeWidth();
-  // this.image.strokeWidth = Math.round(strokeWidth);
 
-// divide tli into gridsPerLine*lines grids
-  // var lines = vertical ? Math.ceil(iwidth / this.strokeWidth) : Math.ceil(iheight / this.strokeWidth);
-  // var gridsPerLine = vertical ? Math.ceil(iheight / this.strokeWidth) : Math.ceil(iwidth / this.strokeWidth);
+// divide tli into row*col grids
   this.grids = this.divideImage();
   var grids = this.grids;
-  var lines = this.linesOfGrids;
-  var gridsPerLine = this.gridsPerLine;
-  //grids: array of lines (vertical if orientation is vertical, vice versa)
-  //       (a line is an array of grids)
+  var rows = this.rows;
+  var cols = this.cols;
+  //grids: array of rows (vertical if orientation is vertical, vice versa)
+  //       (a row is an array of grids)
 
 // // calculate needed probabilities of every grid
-//   grids.map(function(lineOfGrids){
-//     lineOfGrids.map(function(grid){
+//   grids.map(function(rowOfGrids){
+//     rowOfGrids.map(function(grid){
 //       //symbol observation probability
 //       var bp = blackPixels(grid).amount;
 //       grid.obsProb = 1-bp/(strokeWidth^2+1);
@@ -108,73 +104,76 @@ handwrittenImage.prototype.viterbi = function(vertical){
 //     });
 //   });
 
-  var line=0;
+  var row=0;
   var grid=0;  //for loop indexing
+  var col=0;
   var initProb=0;
   var finalProb=0;
-  var oneThird = Math.round(gridsPerLine/3);
-  var twoThirds = Math.round(2*gridsPerLine/3);
-  for(line=0;line<lines;++line){
+  var oneThird = Math.round(cols/3);     //first one third
+  var twoThirds = Math.round(2*cols/3);  //last one third
+  for(row=0;row<rows;++row){
     initProb=0;// i.e., π
     finalProb=0;//i.e., Ɣ
-    for(grid=0;grid<oneThird;++grid){
-      initProb += grids[line][grid].obsProb;
+    for(col=0;col<oneThird;++col){
+      initProb += grids[row][col].obsProb;
     }
     //initial state probability
     initProb /= oneThird;
     // console.log(initProb);
-    for(grid=twoThirds;grid<gridsPerLine;++grid){
-      finalProb += grids[line][grid].obsProb;
+    for(col=twoThirds;col<cols;++col){
+      finalProb += grids[row][col].obsProb;
     }
     //final state probability
-    finalProb /= oneThird;
-    grids[line][gridsPerLine-1].pathProb.p = finalProb;
-    //path probability of first line
-    grids[line][0].pathProb.p = initProb * grids[line][0].obsProb;
-    // console.log(grids[line][0].pathProb.p);
+    finalProb /= oneThird;  //average
+    grids[row][cols-1].pathProb.p = finalProb;
+    //path probability of first row
+    grids[row][0].pathProb.p = initProb * grids[row][0].obsProb;
+    // console.log(grids[row][0].pathProb.p);
   }
 
-  // path probability of central lines
-  var upLine = 0;
-  var thisLine = 0;
-  var downLine = 0;
-  var obliqueTrsProb = (Math.sqrt(2))/2;
-  var calculatePathProb = function(lineOfGrids, line){
-      upLine = (line===0) ? 0 : grids[(line-1)][grid-1].pathProb.p * obliqueTrsProb * lineOfGrids[grid].obsProb;
-      thisLine = lineOfGrids[grid-1].pathProb.p * lineOfGrids[grid].obsProb;
-      downLine = (line===(lines-1)) ? 0 : grids[(line+1)][grid-1].pathProb.p * obliqueTrsProb * lineOfGrids[grid].obsProb;
-      lineOfGrids[grid].pathProb = (thisLine>upLine) ?
-                                   (thisLine>downLine?{"grid":line, "p":thisLine}:{"grid":line+1, "p":downLine}) :
-                                   (upLine>downLine?{"grid":line-1, "p":upLine}:{"grid":line+1, "p":downLine});
-      // console.log(lineOfGrids[grid].pathProb);
+  // path probability of central rows
+  var prevRow = 0;
+  var thisRow = 0;
+  var nextRow = 0;
+  var obliqueTrsProb = (Math.sqrt(2))/2;  // control if oblique path is allowed
+  var calculatePathProb = function(rowOfGrids, row){
+      prevRow = (row===0) ? 0 : grids[(row-1)][col-1].pathProb.p * obliqueTrsProb * rowOfGrids[col].obsProb;
+      thisRow = rowOfGrids[col-1].pathProb.p * rowOfGrids[col].obsProb;
+      nextRow = (row===(rows-1)) ? 0 : grids[(row+1)][col-1].pathProb.p * obliqueTrsProb * rowOfGrids[col].obsProb;
+      rowOfGrids[col].pathProb = (thisRow>=prevRow) ?
+                                   (thisRow>=nextRow?{"prevRow":row, "p":thisRow}:{"prevRow":row+1, "p":nextRow}) :
+                                   (prevRow>=nextRow?{"prevRow":row-1, "p":prevRow}:{"prevRow":row+1, "p":nextRow});
+      // console.log(rowOfGrids[col].pathProb);
   };
-  for(grid=1;grid<gridsPerLine-1;++grid){//excluding last column
+  for(col=1;col<cols-1;++col){//excluding last column
     grids.map(calculatePathProb);
   }
-  // path probability of last line
   c1=this.sw;
   c2=3;
   c3=4;            // magic numbers
   // var thres = 0.1;
   // var thres = Math.pow(1-(c1*c1/(1+(this.sw*this.sw))),c2)*Math.pow(1/Math.sqrt(2),c3);
   var paths = [];
-  grids.map(function(lineOfGrids, line){
-      upLine = (line===0) ? 0 : lineOfGrids[grid].pathProb.p * grids[(line-1)][grid-1].pathProb.p * obliqueTrsProb * lineOfGrids[grid].obsProb;
-      thisLine = lineOfGrids[grid].pathProb.p * lineOfGrids[grid-1].pathProb.p * lineOfGrids[grid].obsProb;
-      downLine = (line===(lines-1)) ? 0 : lineOfGrids[grid].pathProb.p * grids[(line+1)][grid-1].pathProb.p * obliqueTrsProb * lineOfGrids[grid].obsProb;
-      lineOfGrids[grid].pathProb = (thisLine>upLine) ?
-                                   (thisLine>downLine?{"line": line, "grid":line, "p":thisLine}:{"line": line, "grid":line+1, "p":downLine}) :
-                                   (upLine>downLine?{"line": line, "grid":line-1, "p":upLine}:{"line": line, "grid":line+1, "p":downLine});
-      // if(lineOfGrids[grid].pathProb.p > thres){
-        paths.push(lineOfGrids[grid].pathProb);
+  // a path records starting row, point, and its probability
+  
+  grids.map(function(rowOfGrids, row){  //deal with the last column
+      prevRow = (row===0) ? 0 : rowOfGrids[col].pathProb.p * grids[(row-1)][col-1].pathProb.p * obliqueTrsProb * rowOfGrids[col].obsProb;
+      thisRow = rowOfGrids[col].pathProb.p * rowOfGrids[col-1].pathProb.p * rowOfGrids[col].obsProb;
+      nextRow = (row===(rows-1)) ? 0 : rowOfGrids[col].pathProb.p * grids[(row+1)][col-1].pathProb.p * obliqueTrsProb * rowOfGrids[col].obsProb;
+      rowOfGrids[col].pathProb = (thisRow>prevRow) ?
+                                   (thisRow>nextRow?{"thisRow": row, "prevRow":row, "p":thisRow}:{"thisRow": row, "prevRow":row+1, "p":nextRow}) :
+                                   (prevRow>nextRow?{"thisRow": row, "prevRow":row-1, "p":prevRow}:{"thisRow": row, "prevRow":row+1, "p":nextRow});
+      // if(rowOfGrids[col].pathProb.p > thres){
+        paths.push(rowOfGrids[col].pathProb);
       // }
-      // console.log(lineOfGrids[grid].pathProb);
+      // console.log(rowOfGrids[col].pathProb);
   });
-  this.path = paths;
+  this.paths = paths;
+  // this.trackPath();
   this.drawPaths();
 
 
-//   grids[lines].map(function(grid){
+//   grids[rows].map(function(grid){
 // //TODO: calculate path probability (we have set grid.pathProb = finalProb)
 //     grid.pathProb *= 1;
 //     //build path array
@@ -199,26 +198,43 @@ handwrittenImage.prototype.viterbi = function(vertical){
 //TODO: split image by path
 };
 
+handwrittenImage.prototype.trackPath = function(){
+  var cols = this.cols;
+  var grids = this.grids;
+  this.paths.map(function(path){
+    path.route = [];
+    var row = path.thisRow;
+    var col = cols-1;
+    path.route.push({"row":row, "col":col});
+    grids[row][col].pathsThrough.push(path.thisRow);
+    row = path.prevRow;
+    for(col=col-1;col>=0;--col){  //backtracking
+      path.route.push({"row":row, "col":col});
+      grids[row][col].pathsThrough.push(path.thisRow);
+      row = grids[row][col].pathProb.prevRow;
+    }
+    path.route.reverse();
+  });
+};
+
 handwrittenImage.prototype.drawPaths = function(){
-  var stage = 0;
+  var col = 0;
   var context = this.context;
-  var gridsPerLine = this.gridsPerLine;
+  var cols = this.cols;
   var grids = this.grids;
   var sw = this.sw;
   var now = {};
   var next = {};
-  var line = 0;
+  var row = 0;
   var width = this.width;
-  this.path.map(function(p){
+  this.paths.map(function(p){
     now.x = width;
-    // stage = gridsPerLine-1;
-    line = p.line;
-    now.y = sw*(p.line+1)-sw/2;
-    
-    for(stage=gridsPerLine-1;stage>0;--stage){
+    row = p.thisRow;
+    now.y = sw*(p.thisRow+1)-sw/2;
+    for(col=cols-1;col>0;--col){  //backtracking
       next.x = now.x - sw;
-      line = grids[line][stage].pathProb.grid;
-      next.y = sw*(line+1)-sw/2;
+      row = grids[row][col].pathProb.prevRow;
+      next.y = sw*(row+1)-sw/2;
       // console.log(now);
       // console.log(next);
       context.moveTo(now.x, now.y);
@@ -230,9 +246,9 @@ handwrittenImage.prototype.drawPaths = function(){
     }
     // do{
     //   next.x = now.x - sw;
-    //   line = grids[line][stage--].pathProb.grid;
-    //   // stage -= 1;
-    //   next.y = sw*(line+1)-sw/2;
+    //   row = grids[row][col--].pathProb.grid;
+    //   // col -= 1;
+    //   next.y = sw*(row+1)-sw/2;
     //   context.beginPath();
     //         // console.log(now);
     //         // console.log(next);
@@ -242,15 +258,15 @@ handwrittenImage.prototype.drawPaths = function(){
       // context.stroke();
       // now = next;
     // }while(now.x>0);
-    // for(stage = gridsPerLine; stage>0 ; --stage){
-    //   next.x = sw*stage-sw-sw/2;
-    //   next.y = sw*(grids[p.line][stage-1].grid)-sw/2;
+    // for(col = cols; col>0 ; --col){
+    //   next.x = sw*col-sw-sw/2;
+    //   next.y = sw*(grids[p.row][col-1].grid)-sw/2;
     //   context.beginPath();
-    //   // console.log("from %d, %d", stage, p.line);
+    //   // console.log("from %d, %d", col, p.row);
     //   context.moveTo(now.x, now.y);
-    //   // console.log("to %d, %d", stage-1, p.grid);
+    //   // console.log("to %d, %d", col-1, p.grid);
     //   context.lineTo(next.x, next.y);
-    //   // set line color
+    //   // set row color
     //   context.strokeStyle = '#ff0000';
     //   context.stroke();
     //   now = next;
@@ -260,21 +276,20 @@ handwrittenImage.prototype.drawPaths = function(){
 
 
 handwrittenImage.prototype.divideImage = function(){
-// TODO: divide image into m*n parts
-  // var strokeWidth = Math.round(this.strokeWidth());
+// TODO: divide image into row*col grids
   var data = this.original.data;
   var h = this.image.height;
   var w = this.image.width;
   var strokeWidth = this.sw;
   var dividedImages = [];
-  var gridLine = [];
+  var row = [];
   var grid = [];
   var obsProbDenominator = strokeWidth*strokeWidth+1;
   // var obliqueTrsProb = Math.sqrt(2)/2;
   var x=0;
   var y=0;
   for(y=0;y<h;y+=strokeWidth){
-    gridLine=[];
+    row=[];
     for(x=0;x<w;x+=strokeWidth){
       grid = this.getData(x,y,strokeWidth,strokeWidth); //TODO: boundary needs to be concerned
       // grid=[];
@@ -291,13 +306,14 @@ handwrittenImage.prototype.divideImage = function(){
       // grid.trsProb[(y-1).toString()] = obliqueTrsProb;
       // grid.trsProb[(y).toString()] = 1;
       // grid.trsProb[(y+1).toString()] = obliqueTrsProb;
-      // initialize probability of node at specific stage, i.e., δ
+      // initialize probability of node at specific col, i.e., δ
       grid.pathProb = {};
-      gridLine.push(grid);
-    }
-    dividedImages.push(gridLine);
+      grid.pathsThrough = [];
+      row.push(grid);
+    }  //every row contains col grids
+    dividedImages.push(row);
   }
-  this.linesOfGrids = y/strokeWidth;
-  this.gridsPerLine = x/strokeWidth;
+  this.rows = y/strokeWidth;
+  this.cols = x/strokeWidth;
   return dividedImages;
 };
